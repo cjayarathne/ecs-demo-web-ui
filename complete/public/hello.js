a7b4c6aba2b253fd005bcaac6531d734f4a964f2
Chamikara Jayarathne <cjayarathne@virtusa.com>
2015-11-25 13:43:11 -0500

    Docker file

--- complete/public/hello.js
+++ complete/public/hello.js
@@ -1,8 +1,17 @@
+
 $(document).ready(function() {
-    $.ajax({
-        url: "http://rest-service.guides.spring.io/greeting"
-    }).then(function(data) {
-       $('.greeting-id').append(data.id);
-       $('.greeting-content').append(data.content);
-    });
+	$("#submit").click(function() {
+		var name = $('#text').val();
+	    $.ajax({
+	        url: "http://localhost:9000/greeting/?name=" + name
+	        
+	    }).then(function(data) {
+	        
+	       $('.greeting-content').append(data.id + " : " + data.content + "<br/>");
+	       
+	    }, function(data) {
+	    	 $('.greeting-content').append("<font color=\"red\"> Oops... something went wrong. Try again. </font><br/>");
+	    	
+		});
+	});
 });
