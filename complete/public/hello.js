7eac7c3e910c679e3d40731a571fe731231f4658
Chamikara Jayarathne <cjayarathne@virtusa.com>
2015-12-01 10:56:09 -0500

    LB

--- complete/public/hello.js
+++ complete/public/hello.js
@@ -3,8 +3,7 @@ $(document).ready(function() {
 	$("#submit").click(function() {
 		var name = $('#text').val();
 	    $.ajax({
-	        url: "http://api-lb-2069955939.us-east-1.elb.amazonaws.com/greeting/?name=" + name
-	        
+	        url: "http://ecs-demo-api-94384319.us-east-1.elb.amazonaws.com/greeting/?name=" + name
 	    }).then(function(data) {
 	        
 	       $('.greeting-content').append(data.id + " : " + data.content + "<br/>");
